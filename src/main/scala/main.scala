7ff75290b6b2a0d389c65c471a7c76b6cb2860d8
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2023-11-01 15:58:11 +0800

    Update main.scala

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -14,34 +14,51 @@ def main(): Unit = {
   val unitN = ArrayBuffer[String]()
   Nfile.getLines().foreach(line => unitN += line)
 
-  // makes sure the unit file is updated, and removes the """
+  // makes sure the unit file is updated, and removes the double quotation marks
   unitN.foreach(x =>
-    val u = unitN(unitN.indexOf(x)).replace("\"", "")
-    val uPrev = unit(unitN.indexOf(x)).replace("\"", "")
+    val u = unitN(unitN.indexOf(x)).replace("\"", "");
+    val uPrev = unit(unitN.indexOf(x)).replace("\"", "");
     if (!(u.substring(0,u.indexOf(";")) == uPrev.substring(0,uPrev.indexOf(";")))) unit.insert(unitN.indexOf(x),u);
   )
-  var rep = ""
+  var rep = "";
   //println(unit.length)
+
+  // removes unnecessary languages from the file
   unit.foreach(line =>
-    val index = unit.indexOf(line)
+    val index = unit.indexOf(line);
     var cur = unit(index).replace("\"", "");
-    val sec = cur.indexOf(";",cur.indexOf(";")+1);
-    val from = cur.indexOf(";") + 1;
-    val to = cur.indexOf(";", cur.indexOf(";") + 1);
+    val start = cur.indexOf(";") + 1;
+    val sec = cur.indexOf(";",start);
+    //val to = cur.indexOf(";", start);
+
     cur match
       case x if x.contains("_shop;") => {
-        if (sec != -1) {
-          rep = cur.substring(from, to);
-          cur = cur.substring(0, to)
-        }
-        else rep = cur.substring(from)
+        if sec != -1 then
+          rep = cur.substring(start, sec);
+          cur = cur.substring(0, sec);
+        else
+          rep = cur.substring(start);
         unit(index) = cur
       }
       case x if x.contains("_0;") => {
+        if rep == "" && sec != -1 then
+          rep = cur.substring(start,sec)
+        else
+          rep = cur.substring(start)
        // println("yer dad")
+        if sec != -1 then cur = cur.substring(0,sec)
+        unit(index) = cur
       }
-      case x if x.contains("_1;") || x.contains("_2;") => //println("sex")
-      case _ => //println("cock");
+      case x if x.contains("_1;") || x.contains("_2;") => {
+        cur = cur.substring(0,start) + rep;
+        if cur.contains("_2;") then rep = "";
+        unit(index) = cur;
+      }
+      case _ => {
+        println("I hate kids")
+      }
+
+      println(unit(index))
   )
-  println(unit(5848))
+
 }

902c2d278b8abe89df5ec4c6086a4dd1f8bdab9d
Skyline300 <marcushoo2015@gmail.com>
2023-12-28 06:44:38 +0800

    Update main.scala

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,97 +1,39 @@
+package com.skyline.warlangmod
+
 import java.io.FileWriter
 import scala.collection.mutable.ArrayBuffer
 import scala.io.{BufferedSource, Source}
-@main
-def main(): Unit = {
+object Main {
+  def main(args: Array[String]): Unit = {
 
-//  if(args.length < 2)
-//    System.exit(1) // No input file
+    if(args.length < 2)
+      System.exit(1) // No input file
 
+    // Parses modded file
+    val myLangFile: BufferedSource = Source.fromFile(args(0))
+    val parsedModdedLangFile = Language.parse(myLangFile, true)
+    myLangFile.close()
 
-  // adds the file data to the "myLangFileBuffer" array
-  val myLangFile: BufferedSource = Source.fromFile("src/units.csv")
-  val myLangFileBuffer = myLangFile.getLines().toArray.toBuffer
-  myLangFile.close()
+    // Parses update file from WarThunder
+    val updatedLangFile: BufferedSource = Source.fromFile(args(1))
+    val parsedUpdateLangFile = Language.parse(updatedLangFile, false)
+    updatedLangFile.close()
 
-  // adds the new file data into the "updatedLangFileBuffer" array
-  val updatedLangFile: BufferedSource = Source.fromFile("src/unitsN.csv")
-  val updatedLangFileBuffer = updatedLangFile.getLines().toArray.toBuffer
-  updatedLangFile.close()
+    // Inserts the new object from the WarThunder updated language file if there's something
+    parsedUpdateLangFile.languages.zipWithIndex.foreach { case (language, index) =>
+      val moddedTranslationAtIndex = parsedModdedLangFile.languages(index)
+      if(language.objName != moddedTranslationAtIndex.objName && moddedTranslationAtIndex.modded) {
+        val fuckOtherLanguagesUSAUSA = Language(language.objName, language.translations.headOption.toList, false)
+        parsedModdedLangFile.languages.insert(index, fuckOtherLanguagesUSAUSA)
+      }
+    }
 
-  // makes sure the myLangFileBuffer file is updated, and removes the double quotation marks
-  updatedLangFileBuffer.zipWithIndex.foreach { case (line, index) =>
-    val unescapedNewLine = line.replace("\"", "")
-    val unescapedOldLine = myLangFileBuffer(index).replace("\"", "")
-    val firstColumnInUnitN = unescapedNewLine.split(";")(0)
-    val firstColumnInUnit = unescapedOldLine.split(";")(0)
-    if (firstColumnInUnitN != firstColumnInUnit) myLangFileBuffer.insert(index, unescapedNewLine);
+    // Remove stupid duplicates
+    val output = Translations(parsedModdedLangFile.languages.distinctBy(_.objName))
+
+    // Writes all of the updates
+    val w = new FileWriter("src/unitsMod.csv")
+    w.write(output.toString)
+    w.close()
   }
-
-  var i = 0
-  while (i < myLangFileBuffer.size) {
-    var cur = myLangFileBuffer(i).replace("\"", "")
-    val start = cur.indexOf(";") + 1
-    val sec = cur.indexOf(";",start);
-
-    cur match
-      case x if x.contains("_shop;") => {
-        if sec != -1 then cur = cur.substring(0, sec)
-        myLangFileBuffer(i) = cur
-        i += 1
-      }
-      case x if x.contains("_0;") => {
-        if sec != -1 then cur = cur.substring(0,sec)
-        myLangFileBuffer(i) = cur
-        i += 1
-      }
-      case x if x.contains("_1;") || x.contains("_2;") => {
-        if sec != -1 then cur = cur.substring(0, sec)
-        myLangFileBuffer(i) = cur
-        i += 1
-      }
-      case x if x.startsWith(";") => {
-        myLangFileBuffer.remove(i)
-      }
-      case _ => {
-        if (sec != -1) cur = cur.substring(0,sec)
-        myLangFileBuffer(i) = cur
-        i += 1
-      };
-  }
-
-  // removes unnecessary languages from the file
-//  myLangFileBuffer.foreach(line =>
-//    val index = myLangFileBuffer.indexOf(line)
-//    var cur = myLangFileBuffer(index).replace("\"", "")
-//    val start = cur.indexOf(";") + 1
-//    val sec = cur.indexOf(";",start);
-//
-//    cur match
-//      case x if x.contains("_shop;") => {
-//        if sec != -1 then cur = cur.substring(0, sec)
-//        myLangFileBuffer(index) = cur
-//      }
-//      case x if x.contains("_0;") => {
-//        if sec != -1 then cur = cur.substring(0,sec)
-//        myLangFileBuffer(index) = cur
-//      }
-//      case x if x.contains("_1;") || x.contains("_2;") => {
-//        if sec != -1 then cur = cur.substring(0, sec)
-//        myLangFileBuffer(index) = cur
-//      }
-//      case x if x.startsWith(";") => {
-//        myLangFileBuffer.remove(index)
-//      }
-//      case _ => {
-//        if (sec != -1) cur = cur.substring(0,sec)
-//        myLangFileBuffer(index) = cur
-//      };
-//
-//  )
-  val w = new FileWriter("src/unitsMod.csv")
-  for (i <- myLangFileBuffer.indices) {
-    w.write(myLangFileBuffer(i) + "\n")
-  }
-  w.close()
-
 }

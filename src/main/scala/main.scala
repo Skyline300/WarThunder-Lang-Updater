2ce6e138003cecfeb5b85d996b292cb33c994718
Adrian Legaspi <legaspi.adrianking@gmail.com>
2023-12-27 21:58:01 +0200

    Make some refactor

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,91 +1,96 @@
 import java.io.FileWriter
 import scala.collection.mutable.ArrayBuffer
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 @main
-def main(): Unit = {
+def main(args: Array[String]): Unit = {
 
-  // adds the file data to the "unit" array
-  val File = Source.fromFile("src/units.csv")
-  val unit = ArrayBuffer[String]()
-  File.getLines().foreach(line => unit += line)
-  File.close()
+  if(args.length < 2)
+    System.exit(1) // No input file
 
-  // adds the new file data into the "unitN" array
-  val Nfile = Source.fromFile("src/unitsN.csv")
-  val unitN = ArrayBuffer[String]()
-  Nfile.getLines().foreach(line => unitN += line)
-  Nfile.close()
 
-  // makes sure the unit file is updated, and removes the double quotation marks
-  unitN.foreach(x =>
-    val u = unitN(unitN.indexOf(x)).replace("\"", "")
-    val uPrev = unit(unitN.indexOf(x)).replace("\"", "")
-    if (!(u.substring(0,u.indexOf(";")) == uPrev.substring(0,uPrev.indexOf(";")))) unit.insert(unitN.indexOf(x),u);
-  )
+  // adds the file data to the "myLangFileBuffer" array
+  val myLangFile: BufferedSource = Source.fromFile(args[0])
+  val myLangFileBuffer = myLangFile.getLines().toArray.toBuffer
+  myLangFile.close()
+
+  // adds the new file data into the "updatedLangFileBuffer" array
+  val updatedLangFile: BufferedSource = Source.fromFile(args[1])
+  val updatedLangFileBuffer = updatedLangFile.getLines().toArray.toBuffer
+  updatedLangFile.close()
+
+  // makes sure the myLangFileBuffer file is updated, and removes the double quotation marks
+  updatedLangFileBuffer.zipWithIndex.foreach { case (line, index) =>
+    val unescapedNewLine = line.replace("\"", "")
+    val unescapedOldLine = myLangFileBuffer(index).replace("\"", "")
+    val firstColumnInUnitN = unescapedNewLine.split(";")[0]
+    val firstColumnInUnit = unescapedOldLine.split(";")[0]
+    if (!firstColumnInUnitN == firstColumnInUnit) myLangFileBuffer.insert(index, unescapedNewLine);
+  }
+
   var i = 0
-  while (i < unit.size) {
-    var cur = unit(i).replace("\"", "")
+  while (i < myLangFileBuffer.size) {
+    var cur = myLangFileBuffer(i).replace("\"", "")
     val start = cur.indexOf(";") + 1
     val sec = cur.indexOf(";",start);
 
     cur match
       case x if x.contains("_shop;") => {
         if sec != -1 then cur = cur.substring(0, sec)
-        unit(i) = cur
+        myLangFileBuffer(i) = cur
         i += 1
       }
       case x if x.contains("_0;") => {
         if sec != -1 then cur = cur.substring(0,sec)
-        unit(i) = cur
+        myLangFileBuffer(i) = cur
         i += 1
       }
       case x if x.contains("_1;") || x.contains("_2;") => {
         if sec != -1 then cur = cur.substring(0, sec)
-        unit(i) = cur
+        myLangFileBuffer(i) = cur
         i += 1
       }
       case x if x.startsWith(";") => {
-        unit.remove(i)
+        myLangFileBuffer.remove(i)
       }
       case _ => {
         if (sec != -1) cur = cur.substring(0,sec)
-        unit(i) = cur
+        myLangFileBuffer(i) = cur
         i += 1
       };
   }
 
   // removes unnecessary languages from the file
-//  unit.foreach(line =>
-//    val index = unit.indexOf(line)
-//    var cur = unit(index).replace("\"", "")
+//  myLangFileBuffer.foreach(line =>
+//    val index = myLangFileBuffer.indexOf(line)
+//    var cur = myLangFileBuffer(index).replace("\"", "")
 //    val start = cur.indexOf(";") + 1
 //    val sec = cur.indexOf(";",start);
 //
 //    cur match
 //      case x if x.contains("_shop;") => {
 //        if sec != -1 then cur = cur.substring(0, sec)
-//        unit(index) = cur
+//        myLangFileBuffer(index) = cur
 //      }
 //      case x if x.contains("_0;") => {
 //        if sec != -1 then cur = cur.substring(0,sec)
-//        unit(index) = cur
+//        myLangFileBuffer(index) = cur
 //      }
 //      case x if x.contains("_1;") || x.contains("_2;") => {
 //        if sec != -1 then cur = cur.substring(0, sec)
-//        unit(index) = cur
+//        myLangFileBuffer(index) = cur
 //      }
 //      case x if x.startsWith(";") => {
-//        unit.remove(index)
+//        myLangFileBuffer.remove(index)
 //      }
 //      case _ => {
 //        if (sec != -1) cur = cur.substring(0,sec)
-//        unit(index) = cur
+//        myLangFileBuffer(index) = cur
 //      };
 //
 //  )
   val w = new FileWriter("src/unitsMod.csv")
-  for (i <- unit.indices) {
-    w.write(unit(i) + "\n")
+  for (i <- myLangFileBuffer.indices) {
+    w.write(myLangFileBuffer(i) + "\n")
   }
   w.close()
 

6698a381f56cb2d622ebc7f659187c065c8df046
Skyline300 <marcushoo2015@gmail.com>
2023-12-28 07:06:59 +0800

    comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,15 +6,17 @@ import scala.io.{BufferedSource, Source}
 object Main {
   def main(args: Array[String]): Unit = {
 
-    if(args.length < 2)
+    if(args.length < 2) {
+      println("not enough args")
       System.exit(1) // No input file
+    }
 
     // Parses modded file
     val myLangFile: BufferedSource = Source.fromFile(args(0))
     val parsedModdedLangFile = Language.parse(myLangFile, true)
     myLangFile.close()
 
-    // Parses update file from WarThunder
+    // Parses new datamined file or whatnot 
     val updatedLangFile: BufferedSource = Source.fromFile(args(1))
     val parsedUpdateLangFile = Language.parse(updatedLangFile, false)
     updatedLangFile.close()
@@ -23,6 +25,7 @@ object Main {
     parsedUpdateLangFile.languages.zipWithIndex.foreach { case (language, index) =>
       val moddedTranslationAtIndex = parsedModdedLangFile.languages(index)
       if(language.objName != moddedTranslationAtIndex.objName && moddedTranslationAtIndex.modded) {
+                                                                  // headOption because USA USA
         val fuckOtherLanguagesUSAUSA = Language(language.objName, language.translations.headOption.toList, false)
         parsedModdedLangFile.languages.insert(index, fuckOtherLanguagesUSAUSA)
       }

9dc2624b77af46851fa802b6e124e06ed9f8df41
Skyline300 <marcushoo2015@gmail.com>
2024-07-17 20:18:28 +0800

    Update App.scala

--- src/main/scala/com/skyline/warlangmod/App.scala
+++ src/main/scala/com/skyline/warlangmod/App.scala
@@ -4,12 +4,17 @@ import java.io.FileWriter
 
 object App {
   def run(inputFileName: String, originalFile: String, outputFileName: String): Unit = {
+
     val parsingService = ParsingService.instance
     val translationOverwrite = TranslationOverwriteService.instance
     val outputService = Output.instance
+
+
     val moddedTranslations = parsingService.translations(inputFileName)
     val originalUpdatedTranslations = parsingService.translations(originalFile)
     val updatedModdedFile = translationOverwrite.overwrite(originalUpdatedTranslations, moddedTranslations)
+
+
     outputService.write(updatedModdedFile, outputFileName)
   }
 }

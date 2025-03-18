ad66d642aef4341af1067f3eed0f83d60dfb766d
Skyline <marcushoo2015@gmail.com>
2025-03-18 20:31:06 +0800

    gssadfhds f

--- src/main/scala/com/skyline/warlangmod/App.scala
+++ src/main/scala/com/skyline/warlangmod/App.scala
@@ -1,5 +1,8 @@
 package com.skyline.warlangmod
 
+import com.skyline.warlangmod.cli.Params.TranslationFileType
+import scala.util.Try
+
 object App:
   def run(
       inputFileName: String,
@@ -11,8 +14,26 @@ object App:
     val translationOverwrite = TranslationOverwriteService.instance
     val outputService = Output.instance
 
+
     val moddedTranslations = parsingService.translations(inputFileName)
     val originalUpdatedTranslations = parsingService.translations(originalFile)
     val updatedModdedFile = translationOverwrite.overwrite(originalUpdatedTranslations,moddedTranslations)
 
     outputService.write(updatedModdedFile, outputFileName)
+
+
+  def runOnline(inputFileName: String, filePath: String, fileType:TranslationFileType):Unit =
+    val parsingService = ParsingService.instance
+    val translationOverwrite = TranslationOverwriteService.instance
+    val outputService = Output.instance
+    fileType match {
+      case TranslationFileType.Units => ???
+      case TranslationFileType.Weaponry => ??? //What the fuck am i doing
+    }
+//    Try {
+//      FetchService.fetchFile(filePath)
+//    }
+
+    val moddedTranslations = parsingService.translations(inputFileName)
+
+
